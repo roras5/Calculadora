0459ab8e3f7361cfd3e7ea17057bdf1e1830ad36
Jose Rios <roraj7759@gmail.com>
2020-12-30 12:26:26 -0600

    Añadi los menus de la calculadora

--- Conversiones.java
+++ Conversiones.java
@@ -1,3 +1,34 @@
 public class Conversiones {
     
+    private double metros;
+    private double centimetros =4.2;
+    private double kilometro;
+    private double milimetros;
+
+
+    public void setMetros(double m){
+
+        this.metros = m;
+    }
+
+   public double metrosAcentimetros(){
+
+        if(metros == 0){
+
+            System.out.println("El valor debe ser diferente de cero");
+        }else{
+
+            centimetros = metros*100;
+            
+        }
+        return this.centimetros;
+       
+
+
+   }
+
+
+
+
+    
 }
---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+
+
+    public static void main(String[] args) {
+
+        String menu_principal [] = {"Metros","Centimetros","Kilometros", "Milimetros"};
+
+        String metrosA [] = {"Metros a cm ", "Metros a km", "Metros a mm"};
+        String centimetrosA [] = {"Centimetros a m", "Centimetros a Km","Centimetros a mm"};
+        String kilometrosA [] ={"Kilometros a m", "Kilometros a cm", "Kilometros a mm"};
+        String milimetrosA [] = {"Milimetros a m", "Milimetros a cm", "Milimetros a Kilometros"};
+
+
+       
+
+        
+    }
+
+
+    /*
+    Teniendo en cuenta que queda poco tiempo para realizarlo, el proyecto será una simple calculadora de conversiones entre unidades métricas. El usuario debe poder introducir una cantidad, la unidad original y la unidad destino y el sistema debe calcular la conversión y reportar el resultado. Las unidades a manejar son: m, cm, mm, Km, millas, pulgadas, pies. El proyecto se entrega a través de Tegel y tienen como máximo para la entrega la fecha del ordinario.  
+    */
+
+
+
+}
