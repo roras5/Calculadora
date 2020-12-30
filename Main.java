08fa0ac0ae71689b48b1e2ec6a1242dfba7f5df7
Ramiro <vegaramiro02mail.com>
2020-12-30 13:22:01 -0600

    actualizacion de menu

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-import java.util.*;
-public class Main{
+
+public class Main {
+
     public static void main(String[] args) {
         //menu de opciones
         //faltan unidades metricas ojo
@@ -28,7 +29,6 @@ public class Main{
 
             case 1://Metros a...
 
-<<<<<<< HEAD
                 //===============menu de metros====================
                 menuMetros.setTitulo("A que unidad decia convertir los metros");
                 menuMetros.muestraMenu();
@@ -47,89 +47,6 @@ public class Main{
                         
                     default:
                         System.out.println("Opcion invalida");
-=======
-        Scanner scan = new Scanner(System.in);
-
-        Conversiones objeto = new Conversiones();
-
-        String menu_principal [] = {"Metros","Centimetros","Kilometros", "Milimetros","Salir"};
-        String metrosA [] = {"Metros a cm ", "Metros a km", "Metros a mm"};
-        String centimetrosA [] = {"Centimetros a m", "Centimetros a Km","Centimetros a mm"};
-        String kilometrosA [] ={"Kilometros a m", "Kilometros a cm", "Kilometros a mm"};
-        String milimetrosA [] = {"Milimetros a m", "Milimetros a cm", "Milimetros a Kilometros"};
-
-        Menus menu_p = new Menus(); // Menu principal
-        Menus menu_m = new Menus(); // Menu metros A
-        Menus menu_c = new Menus(); // Menus centimetros A
-        
-
-        menu_p.setTitulo("Bienvenidos");
-        menu_p.setOpciones(menu_principal);
-        menu_p.muestraMenu();
-
-       //menu_dinamico.getOpcion();
-
-
-        switch(menu_p.getOpcion()){
-
-            case 1: 
-            //Conversion de metros a otras unidades
-                menu_m.setTitulo("Metros A ");
-                menu_m.setOpciones(metrosA);
-                menu_m.muestraMenu();
-
-                
-                switch (menu_m.getOpcion()) { 
-                    case 1:  // Metros a centimetros
-                    System.out.println("Ingrese los metros");
-                    double aux = scan.nextDouble();
-                    objeto.setMetros(aux);
-
-                    System.out.println(objeto.metrosAcentimetros()+"cm");
-                    
-                    
-                    break;
-
-                    case 2: // Metros a kilometros
-                    
-                    break;
-
-                    case 3: // Metros a milimetros
-                    
-                    break;
-
-                    default: 
-                    System.out.println("Esa no te la vengo manejando krnal");
-                    
-                    break;
-                    
-                }
-            
-            break;
-
-            case 2:  break;
-
-            case 3: break;
-
-            case 4: break;
-
-            case 5: 
-                System.exit(0);
-            break;
-
-            default:
-                System.out.println("Esa no la tengo carnal");
-                break;
-
-        } //Final del programa "SWITCH"
-
-
-       
-
-
-
-
->>>>>>> d59725913b1fd86d8a9df70496b23d85aff52326
 
                 }
 
@@ -218,5 +135,4 @@ public class Main{
 
     }
 
-        
 }
